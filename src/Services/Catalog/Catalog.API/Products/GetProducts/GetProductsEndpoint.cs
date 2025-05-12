acd07f0b4d759571a38af1ca4d6da101bed51e67
Arnold FOSSO <arnoldprosperfosso237@gmail.com>
2025-05-12 14:58:02 +0100

    Ajout des fonctionnalités listing des produits et récupération d'un produit par son Id

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products", async (ISender sender) =>
+            {
+                var products = await sender.Send(new GetProductsQuery());
+                var response = products.Adapt<GetProductsResponse>();
+
+                return TypedResults.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+}
